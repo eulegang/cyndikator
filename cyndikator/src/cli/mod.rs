cd12c539bd5cbdcb2d27de286262696103c12d75
eulegang <eulegang@eulegang.dev>
2021-02-15 23:28:13 -0600

    Removing add/ls commands

--- cyndikator/src/cli/mod.rs
+++ cyndikator/src/cli/mod.rs
@@ -1,15 +1,11 @@
 use structopt::StructOpt;
 
-mod add;
 mod init;
-mod ls;
 mod run;
 mod track;
 
 #[derive(StructOpt)]
 pub enum Cli {
-    Ls(ls::Ls),
-    Add(add::Add),
     Run(run::Run),
     Track(track::Track),
     Tracking(track::Tracking),
@@ -20,8 +16,6 @@ pub enum Cli {
 impl Cli {
     pub async fn run(self) -> eyre::Result<()> {
         match self {
-            Cli::Ls(cmd) => cmd.run().await,
-            Cli::Add(cmd) => cmd.run().await,
             Cli::Run(cmd) => cmd.run().await,
             Cli::Track(cmd) => cmd.run().await,
             Cli::Tracking(cmd) => cmd.run().await,
