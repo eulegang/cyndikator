83c63b1f3ac4ac8791483e26a7857299f4eebdc8
eulegang <eulegang@eulegang.dev>
2025-11-10 22:52:52 -0600

    adding an initial types for lua documentation

---
+++ conf/types.lua
@@ -0,0 +1,68 @@
+---@meta
+
+--- @class Entry
+--- @field id string
+--- @field title string | nil
+--- @field authors Person[]
+--- @field contributors Person[]
+--- @field summary string | nil
+--- @field content Content | nil
+--- @field source string | nil
+--- @field categories Category[]
+--- @field links Link[]
+--- @field base string | nil
+local Entry = {}
+
+--- @param tag string
+function Entry:has_category(tag)
+end
+
+--- @class Feed
+--- @field id string
+--- @field title string | nil
+--- @field description string | nil
+--- @field authors Person[]
+--- @field contributors Person[]
+--- @field links Link[]
+--- @field categories Category[]
+--- @field ttl number | nil
+local Feed = {}
+
+--- @param tag string
+function Feed:has_category(tag)
+end
+
+--- @class Person
+--- @field name string
+--- @field uri string | nil
+--- @field email string | nil
+
+--- @class Category
+--- @field term string
+--- @field label string | nil
+--- @field subcategories Category[]
+
+--- @class Link
+--- @field href string
+--- @field rel string | nil
+--- @field media_type string | nil
+--- @field title string | nil
+
+--- @class Content
+--- @field type "body" | "link"
+--- @field body string | nil
+--- @field link Link | nil
+
+--- Report to the user using a notification system
+function report() end
+
+--- Record the current event in a database
+function record() end
+
+--- Log the message
+--- @param msg string
+function log(msg) end
+
+--- Run a command
+--- @param cmd string
+function exec(cmd) end
